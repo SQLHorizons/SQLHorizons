8d20ee98902ad7226b234cbd55e33df802340140
pauldmax <paul.dmax@outlook.com>
2016-09-30 09:00:39 +0100

    Install-DscResource.ps1
    Zips up a PSGallery DSC Resource for deployment to the DSC pull server.

---
+++ Install-DscResource.ps1
@@ -0,0 +1,18 @@
+﻿Function Install-DscResource
+{
+       [CmdletBinding(DefaultParameterSetName = "Default", SupportsShouldProcess = $true)]
+       Param
+        (
+            [parameter(Mandatory = $true)]
+            [ValidateNotNullOrEmpty()]
+            [string]$Source,
+            [string]$Destination = "C:\Program Files\WindowsPowerShell\DscService\Modules"
+        )
+   
+    $Version        = (Get-ChildItem $Source).Name
+    $ModuleName     = $Destination + '\' + (Get-ChildItem $Source).Parent.Name + '_' + $Version
+    $DestinationZip = $ModuleName +'.zip' 
+ 
+    Compress-Archive -Path "$Source\$Version\*" -DestinationPath $DestinationZip
+    New-DscChecksum $DestinationZip
+}
