9bdfbe67d573a1af7e878625f653b13d6c7b35a6
Paul Maxfield <paul.dmax@outlook.com>
2016-11-22 14:50:15 +0000

    Create Test.Update-PingFiles.ps1

---
+++ Server/Modules/Tests/Test.Update-PingFiles.ps1
@@ -0,0 +1,6 @@
+$parameters = @{
+    Server = "A-Server01"
+    Path   = "\\sqlhorizons.com\FS\Collaboration\ICT\Dept\Working\Infrastructure\Servers\Patching\Text Files"
+}
+
+Update-PingFiles @parameters
