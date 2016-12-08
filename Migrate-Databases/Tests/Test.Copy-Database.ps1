cd9506547266b03756a312dc4366fc831f69a1c4
Paul Maxfield <paul.dmax@outlook.com>
2016-12-08 13:55:45 +0000

    Update Test.Copy-Database.ps1

--- Migrate-Databases/Tests/Test.Copy-Database.ps1
+++ Migrate-Databases/Tests/Test.Copy-Database.ps1
@@ -1,7 +1,7 @@
 Get-SQLPSModule
 
-$srcSrv         = 'DB-OC34' #'DB-OCC04QC\C'  #'DB-OCC04QB\B'
-$tgtSrv         = 'DB-OC46'       #'DB-OC46'
-$dbName         = 'NdiCdr_SC' #'ThreatsToLife' #'Signpost_Audit', 'ThreatsToLife' #'COMPTrain' #'COMPACT'  #
+$srcSrv         = 'DB-OC01'
+$tgtSrv         = 'DB-OC02'
+$dbName         = 'SQLInfo'
 
 Copy-Database -Source $srcSrv -Destination $tgtSrv -Database $dbName -Verbose 
