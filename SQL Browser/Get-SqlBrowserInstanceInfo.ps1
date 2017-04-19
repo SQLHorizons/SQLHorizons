26076cd6a5cd16c3219533c7dab2979ab747a3ec
Paul Maxfield <paul.dmax@outlook.com>
2017-04-19 14:59:35 +0100

    Update Get-SqlBrowserInstanceInfo.ps1

--- SQL Browser/Get-SqlBrowserInstanceInfo.ps1
+++ SQL Browser/Get-SqlBrowserInstanceInfo.ps1
@@ -15,53 +15,53 @@ Get-SqlBrowserInstanceInfo $env:COMPUTERNAME
 The name or IP Address of the server.
 .PARAMETER $InstanceName
 The name of the SQL Instance.    #>
-	[CmdletBinding(SupportsShouldProcess = $False)]
-	param
-	(
-		[Parameter(Mandatory = $True, ValueFromPipeLine = $True)]
-		[string]$ServerName,
-		[Parameter(Mandatory = $True, ValueFromPipeLine = $False)]
-		[string]$InstanceName
-	)
-	
-	process
-	{
-		$instances = @()
-		[System.Net.IPAddress]$ipAddress = $null
-		
-		$ipAddress = [System.Net.Dns]::GetHostAddresses($serverName) | Select -First 1
-		
-		if ($ipAddress -ne $null)
-		{
-			[System.Net.IPEndPoint]$ipEndPoint = New-Object System.Net.IPEndPoint($ipAddress, 1434)
-			[System.Net.Sockets.UdpClient]$udpClient = New-Object System.Net.Sockets.UdpClient
-			$udpClient.Client.ReceiveTimeout = 10000
-			
-			$instanceNameData = [System.Text.Encoding]::Default.GetBytes($instanceName)
-			[byte[]]$requestData = @(0x04) + $instanceNameData + 0x00
-			[byte[]]$responseData = $null
-			
-			try
-			{
-				$udpClient.Connect($ipEndPoint)
-				
-				$bytesSent = $udpClient.Send($requestData, $requestData.Length)
-				
-				$responseData = do
-				{
-					$udpClient.Receive([ref]$ipEndPoint)
-				}
-				while ($udpClient.Available -gt 0)
-			}
-			finally
-			{
-				$udpClient.Close()
-				$udpClient.Dispose()
-			}
-			
-			$instances = Parse-ServerResponse $responseData
-		}
-		
-		return $instances
-	}
+    [CmdletBinding(SupportsShouldProcess = $False)]
+    param
+    (
+        [Parameter(Mandatory = $True, ValueFromPipeLine = $True)]
+        [string]$ServerName,
+        [Parameter(Mandatory = $True, ValueFromPipeLine = $False)]
+        [string]$InstanceName
+    )
+    
+    process
+    {
+        $instances = @()
+        [System.Net.IPAddress]$ipAddress = $null
+        
+        $ipAddress = [System.Net.Dns]::GetHostAddresses($serverName) | Select -First 1
+        
+        if ($ipAddress -ne $null)
+        {
+            [System.Net.IPEndPoint]$ipEndPoint = New-Object System.Net.IPEndPoint($ipAddress, 1434)
+            [System.Net.Sockets.UdpClient]$udpClient = New-Object System.Net.Sockets.UdpClient
+            $udpClient.Client.ReceiveTimeout = 10000
+            
+            $instanceNameData = [System.Text.Encoding]::Default.GetBytes($instanceName)
+            [byte[]]$requestData = @(0x04) + $instanceNameData + 0x00
+            [byte[]]$responseData = $null
+            
+            try
+            {
+                $udpClient.Connect($ipEndPoint)
+                
+                $bytesSent = $udpClient.Send($requestData, $requestData.Length)
+                
+                $responseData = do
+                {
+                    $udpClient.Receive([ref]$ipEndPoint)
+                }
+                while ($udpClient.Available -gt 0)
+            }
+            finally
+            {
+                $udpClient.Close()
+                $udpClient.Dispose()
+            }
+            
+            $instances = Parse-ServerResponse $responseData
+        }
+        
+        return $instances
+    }
 }
