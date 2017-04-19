fbc6313567f35de5d7f364488c6e8cc04998faec
Paul Maxfield <paul.dmax@outlook.com>
2017-04-19 14:55:51 +0100

    Update Parse-ServerResponseDac.ps1

--- SQL Browser/Parse-ServerResponseDac.ps1
+++ SQL Browser/Parse-ServerResponseDac.ps1
@@ -1,27 +1,27 @@
 function Parse-ServerResponseDac([byte[]]$responseData)
 {
-	$dacPort = 0
-	
-	if (($responseData -ne $null) -and ($responseData[0] -eq 0x05))
-	{
-		$responseSize = [System.BitConverter]::ToUInt16($responseData, 1)
-		
-		if (($responseData.Length -eq 6) -and ($responseSize -eq 6))
-		{
-			if ($responseData[3] -eq 0x01)
-			{
-				$dacPort = [System.BitConverter]::ToUInt16($responseData, 4)
-			}
-			else
-			{
-				Write-Error "An unexpected protocol version was returned. Expected 0x01 but got $($requestData[3])."
-			}
-		}
-		else
-		{
-			Write-Error "The response size was incorrect."
-		}
-	}
-	
-	return $dacPort
+    $dacPort = 0
+    
+    if (($responseData -ne $null) -and ($responseData[0] -eq 0x05))
+    {
+        $responseSize = [System.BitConverter]::ToUInt16($responseData, 1)
+        
+        if (($responseData.Length -eq 6) -and ($responseSize -eq 6))
+        {
+            if ($responseData[3] -eq 0x01)
+            {
+                $dacPort = [System.BitConverter]::ToUInt16($responseData, 4)
+            }
+            else
+            {
+                Write-Error "An unexpected protocol version was returned. Expected 0x01 but got $($requestData[3])."
+            }
+        }
+        else
+        {
+            Write-Error "The response size was incorrect."
+        }
+    }
+    
+    return $dacPort
 }
