17c4e0f25ddc312e5ddc24a5c27c08ded1152335
Paul Maxfield <paul.dmax@outlook.com>
2018-02-11 20:18:01 +0000

    Create config_cntl_2.10.ps1
    The following configuration control disables unnecessary SQL server protocols during the SQL installation.

---
+++ CISCAT/config_cntl_2.10.ps1
@@ -0,0 +1,20 @@
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Force
+
+Import-Module -Name 'SQLPS' -DisableNameChecking
+
+$SQLServer = $env:COMPUTERNAME 
+$srvobj = New-Object -TypeName Microsoft.SqlServer.Management.SMO.Wmi.ManagedComputer($SQLServer)
+
+foreach($ClientProtocol in $srvobj.ClientProtocols){
+    $protocol = $srvobj.GetSmoObject($ClientProtocol.Urn.Value.Replace("/Client","/ServerInstance[@Name='MSSQLSERVER']/Server"))
+
+    
+
+    if($protocol.Name -iin ("sm","tcp")){
+        $protocol.IsEnabled = $true
+    }
+    else{
+        $protocol.IsEnabled = $false
+    }
+    $protocol.Alter()
+}
