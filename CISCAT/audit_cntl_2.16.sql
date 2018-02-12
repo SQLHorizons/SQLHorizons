21ce6e86d2dad5611a74eea0bf160d764cf7b73a
Paul Maxfield <paul.dmax@outlook.com>
2018-02-12 15:33:43 +0000

    Create audit_cntl_2.16.sql

---
+++ CISCAT/audit_cntl_2.16.sql
@@ -0,0 +1,11 @@
+ALTER TRIGGER [audit_cntl_2.16]
+ON ALL SERVER   
+FOR ALTER_DATABASE   
+AS
+    IF EXISTS (
+    SELECT 1
+       WHERE
+       EVENTDATA().value('(/EVENT_INSTANCE/TSQLCommand/CommandText)[1]', 'NVARCHAR(100)')
+       LIKE '% AUTO_CLOSE ON WITH NO_WAIT'
+       )
+       ROLLBACK;
