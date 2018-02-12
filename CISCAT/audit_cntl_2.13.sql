c4c82f75c25610d551f8c6860f90b7a7e4aae9c7
Paul Maxfield <paul.dmax@outlook.com>
2018-02-12 14:41:08 +0000

    Update audit_cntl_2.13.sql

--- CISCAT/audit_cntl_2.13.sql
+++ CISCAT/audit_cntl_2.13.sql
@@ -1,11 +1,17 @@
 ALTER TRIGGER [audit_cntl_2.13]
-ON ALL SERVER   
-FOR DDL_SERVER_LEVEL_EVENTS   
-AS  
-    IF EXISTS (
-    SELECT 1
-       WHERE
-       EVENTDATA().value('(/EVENT_INSTANCE/PropertyName)[1]', 'NVARCHAR(100)')
-       = 'remote access'
-       )
-       ROLLBACK;
+ON ALL SERVER   
+FOR DDL_SERVER_LEVEL_EVENTS   
+AS  
+    IF EXISTS (
+    SELECT 1
+       WHERE
+       EVENTDATA().value('(/EVENT_INSTANCE/SID)[1]', 'NVARCHAR(100)')
+       = 'AQ=='
+	   AND
+	   EVENTDATA().value('(/EVENT_INSTANCE/EventType)[1]', 'NVARCHAR(100)')
+	   = 'ALTER_LOGIN'
+	   AND
+	   EVENTDATA().value('(/EVENT_INSTANCE/TSQLCommand/CommandText)[1]', 'NVARCHAR(100)')
+	   LIKE '%ENABLE%'
+       )
+       ROLLBACK;
