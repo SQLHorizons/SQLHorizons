5a6da8f61920495f0169213b7f51a7ec1f445fd1
Paul Maxfield <paul.dmax@outlook.com>
2018-03-01 13:28:39 +0000

    Create audit_cntl_2.4.ps1

---
+++ CISCAT/audit_cntl_2.4.ps1
@@ -0,0 +1,44 @@
+##  Input parameters?
+param(
+    [string]$server = "(local)"
+)
+
+##  Load dependent modules.
+Import-Module AWSPowerShell, SQLPS -DisableNameChecking -ErrorAction Stop
+
+$SQLsrv = New-Object Microsoft.SqlServer.Management.Smo.Server($server)
+
+#######################  apply: audit_cntl_2.4 trigger  #######################
+
+$trigger = "audit_cntl_2.4"
+$SQLsrv.Refresh()
+
+if(!($SQLsrv.Triggers.Item($trigger))){
+    $trg = New-Object Microsoft.SqlServer.Management.Smo.ServerDdlTrigger
+    $trg.Parent = $SQLsrv
+    $trg.Name = $trigger
+
+    $trg.TextHeader = "
+    CREATE TRIGGER [$trigger]
+    ON ALL SERVER
+    FOR DDL_SERVER_LEVEL_EVENTS
+    AS"
+
+    $trg.TextBody = "
+
+    IF EXISTS (
+    SELECT 1
+      WHERE
+      EVENTDATA().value('(/EVENT_INSTANCE/PropertyName)[1]','NVARCHAR(MAX)')
+      = 'Database Mail XPs'
+      AND
+      EVENTDATA().value('(/EVENT_INSTANCE/PropertyValue)[1]','NVARCHAR(MAX)')
+      = 1
+      )
+      ROLLBACK;
+    "
+
+    $trg.Create()
+
+    Clear-Variable trg
+}
