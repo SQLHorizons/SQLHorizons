49ac760f669330c5004818e9b5a58ec9c74d9dcc
Paul Maxfield <paul.dmax@outlook.com>
2018-05-13 22:31:07 +0100

    Update DacServices.Register.ps1

--- dac/DacServices.Register.ps1
+++ dac/DacServices.Register.ps1
@@ -1,1 +1,36 @@
-e
+$targetDatabaseName = @{
+    Name        = "trgDBName"
+    Description = "The name of the database for which to add registration information."
+    Value       = "MasterSQLPlus"
+}
+Set-Variable @targetDatabaseName
+
+$applicationName = @{
+    Name        = "appName"
+    Description = "The application name to be stored in the DAC metadata."
+    Value       = "MasterSQLPlus"
+}
+Set-Variable @applicationName
+
+$applicationVersion = @{
+    Name        = "appVersion"
+    Description = "The version number to be stored in the DAC metadata."
+    Value       = $([System.Version]"1.0.0.0")
+}
+Set-Variable @applicationVersion
+
+$applicationDescription = @{
+    Name        = "appDesc"
+    Description = "The application description to be stored in the DAC metadata."
+    Value       = ""
+}
+Set-Variable @applicationDescription
+
+###################  DacServices.Register Method Variables  ###################
+
+$trgDBName.GetType()
+$appName.GetType()
+$appVersion.GetType()
+$appDesc.GetType()
+
+####################################  END  ####################################
